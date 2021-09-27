2b975c6f302a5c8aad91d4f3498b77aae4876681
Björn Tirsén <bjorntirsen@gmail.com>
2021-09-27 13:37:35 +0200

    :sparkles: Add todo list and items

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const todoItems = [
+  {
+    id: 1,
+    title: 'First item',
+    content: 'Content of first item.',
+  },
+  {
+    id: 2,
+    title: 'second item',
+    content: 'Content of second item.',
+  },
+  {
+    id: 3,
+    title: 'third item',
+    content: 'Content of third item.',
+  },
+];
+
+/* GET all items. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Björns TODO', items: todoItems });
+});
+
+/* GET one item. */
+router.get('/items/:id', function (req, res, next) {
+  const itemId = parseInt(req.params.id);
+  const item = todoItems.find((item) => item.id === itemId);
+  res.render('item', { title: 'Björns TODO', item });
 });
 
 module.exports = router;
